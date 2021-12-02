dba660bf1e35be7029b25dc9aee14ecf4c94a119
bestdocnihad <nihad@bestdocapp.com>
2021-12-02 21:36:39 +0530

    j

--- index.js
+++ index.js
@@ -2,15 +2,10 @@
 
 const Joi =require('joi');
 
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync(__dirname+'/sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync(__dirname+'/sslcert/server.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
+
+
 
 
 // app.use(express.json());
@@ -60,15 +55,15 @@ else{
 });
 
 
-
+const port =process.env.PORT || 3000;
+app.listen(port,()=>{
+   
+    console.log(`listening on port ${port}`)
+});
 
 
 // your express configuration here
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
 
 
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
